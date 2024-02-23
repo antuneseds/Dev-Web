ed42d79e470e21d9cb8c24c345a750b1ed065f0a
antuneseds <esterdossantosantunes04@gmail.com>
2024-02-23 11:45:43 -0300

    contador

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,32 @@ import { ref } from 'vue'
 
 const contador = ref(0)
 
+function soma() {
+  contador.value++
+}
+
 function decrementar() {
-  contador.value--
+  if (contador.value > 0) contador.value--
+}
+
+function resetar() {
+  contador.value = 0
 }
 
 </script>
 
 <template>
-  <button @click="contador++">+</button>
+  <button @click="soma">+</button>
   <button @click="decrementar">-</button>
+  <button @click="resetar">0</button>
   <p>Valor do contador: {{ contador }}</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+  button {
+    font-weight: bold;
+    background-color: pink;
+    color: black;
+    padding: 10px;
+  }
+</style>
